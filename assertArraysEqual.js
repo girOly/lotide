ea2729c88a5fff0f6c7fba2aa24e2f9b8e07df2c
girOly <girardob@gmail.com>
2019-06-25 23:53:57 +0000

    AssertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) == true) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+//
+//
+//
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (var i = 0; i <= arrayOne.length; i++) {
+    console.log(arrayOne[i])
+    console.log(arrayTwo[i])
+ if (arrayOne[i] !== arrayTwo[i]) {
+      return false
+}} return true
+}
