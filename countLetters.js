59f1e1f5bacd154ce981e7bc177153d3531e68d4
girOly <girardob@gmail.com>
2019-06-26 18:11:02 +0000

    countLetters Function

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// // TEST CODE
+//
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1,1).split(' ').join('');
+
+const countLetters = function(string) {
+  var noSpaces = string.split(" ").join("");
+  const objectLetters = {};
+  for (letter of noSpaces) {
+    // console.log(objectLetters[letter])
+    if (objectLetters[letter]) {
+      // if (objectLetters[letter]) {
+      objectLetters[letter] += 1;
+    } else {
+      objectLetters[letter] = 1;
+    }
+    // console.log(letter);
+  }
+
+  return objectLetters;
+};
+
+console.log(countLetters("lighthouse in the house"));
