0cc239a3e0700504c20efe25b144e533f91b6b10
girOly <girardob@gmail.com>
2019-06-26 21:47:45 +0000

    Fixed loop ending early

--- eqObjects.js
+++ eqObjects.js
@@ -24,21 +24,30 @@ const eqObjects = function(object1, object2) {
   var objectListOne = Object.keys(object1);
   var objectListTwo = Object.keys(object2);
   if (objectListOne.length != objectListTwo.length) {
+    console.log("Ending at Line 27");
     return false;
   } else {
     for (key of objectListOne) {
-      if (objectListOne[key] != objectListTwo[key]) {
+      console.log("Ending at Line 31"[key]);
+      if (Array.isArray(objectListOne[key]) == true) {
+        console.log("Loop ending at Line 33");
+        return eqArrays(objectListOne, objectListTwo);
+      }else{
+        if (objectListOne[key] !== objectListTwo[key]) {
+        console.log("Loop ending at Line 37" + [key]);
         return false;
-      } else {
-        return true;
+      // } else {
+      //   console.log("Loop ending at line 40");
+      //   return false;
       }
+      return true
     }
   }
-};
+}}
 // ______________________________________________________________________________
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
+const abc = { a: "1", b: "2",};
 console.log(eqObjects(ab, abc)); // => false
