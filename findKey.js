d72bf0e5ce47b1b9fab1550170be6f4c113bedc5
girOly <girardob@gmail.com>
2019-06-27 22:22:49 +0000

    FindKey Functiom

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const findKey = function(object, callback) {
+  let foundKey = [];
+  for (key in object) {
+    // console.log(key);
+    // console.log(object[key]);
+    if (callback(object[key])) {
+      foundKey = [key];
+      return foundKey;
+    }
+  }
+};
+
+console.log(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 }
+    },
+    x => x.stars === 2
+  )
+); // => "noma"
