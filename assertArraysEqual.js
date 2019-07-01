af5ff3a4be8cec45205030f884864f197e67c266
girOly <girardob@gmail.com>
2019-07-01 18:06:09 +0000

    Pair Coding - Code rework with Jess

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,29 @@
-const assertEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) == true) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+// const assertEqual = function(actual, expected) {
+//   if (eqArrays(actual, expected) == true) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 //
 //
 //
 
-const eqArrays = function(arrayOne, arrayTwo) {
+const assertArraysEqual = function(arrayOne, arrayTwo) {
   for (var i = 0; i <= arrayOne.length; i++) {
-    console.log(arrayOne[i]);
-    console.log(arrayTwo[i]);
+    // console.log(arrayOne[i]);
+    // console.log(arrayTwo[i]);
     if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
   }
   return true;
 };
+
+
+
+
+
+
+module.exports = assertArraysEqual;
