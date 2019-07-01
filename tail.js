75b172f4aeb3a96cb5011c306a30ce11c0e7d3c1
girOly <girardob@gmail.com>
2019-07-01 17:48:56 +0000

    Pair coding - Code rework with Jess

--- tail.js
+++ tail.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 const tail = function(element, elementTwo) {
   return elementTwo;
 };
@@ -18,3 +19,5 @@ const tail = function(element, elementTwo) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
