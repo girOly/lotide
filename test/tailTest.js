e8600c9ee27d56d10ae5472052637d5ec95de3db
girOly <girardob@gmail.com>
2019-07-01 20:58:22 +0000

    Pair Coding - Code rework with Jess

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,17 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
+describe("#tail", () => {
+  // it("returns 1 for [1, 2, 3]", () => {
+  //   assert.DeepEqual(tail([1, 2, 3]), 1);
+  // });
+
+  it("returns '5' for ['4, 5']", () => {
+    assert.deepEqual(tail("4", "5"), "5");
+  });
+});
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
